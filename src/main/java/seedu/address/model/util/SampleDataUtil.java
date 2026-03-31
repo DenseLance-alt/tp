fc181a5a899348b5d1c1c9028fcee48f5c43dcd1
DenseLance-alt <e1406571@u.nus.edu>
2026-03-31 20:52:47 +0800

    Add more samples

--- src/main/java/seedu/address/model/util/SampleDataUtil.java
+++ src/main/java/seedu/address/model/util/SampleDataUtil.java
@@ -25,14 +25,24 @@ public class SampleDataUtil {
 
     /** Sample delivery objects */
     private static final Delivery SAMPLE_DELIVERY_ALEX = new Delivery(
-            new StartDate("2026-10-21"), new EndDate("2026-12-20"),
+            new StartDate("2026-09-20"), new EndDate("2026-12-20"),
             getDeliveryDaySet("Monday", "Tuesday", "Wednesday", "Friday", "Sunday"),
             new DeliveryTime("16:00")
     );
     private static final Delivery SAMPLE_DELIVERY_BERNICE = new Delivery(
-            new StartDate("2026-10-09"), new EndDate("2026-12-21"),
-            getDeliveryDaySet("Monday", "Wednesday", "Friday"),
-            new DeliveryTime("16:00")
+            new StartDate("2026-08-09"), new EndDate("2027-02-09"),
+            getDeliveryDaySet("Monday", "Thursday", "Friday", "Saturday"),
+            new DeliveryTime("16:30")
+    );
+    private static final Delivery SAMPLE_DELIVERY_IRFAN = new Delivery(
+            new StartDate("2025-01-01"), new EndDate("2050-01-01"),
+            getDeliveryDaySet("Monday", "Tuesday", "Wednesday", "Thursday", "Friday"),
+            new DeliveryTime("16:45")
+    );
+    private static final Delivery SAMPLE_DELIVERY_ROY = new Delivery(
+            new StartDate("2020-12-25"), new EndDate("2040-12-25"),
+            getDeliveryDaySet("Saturday", "Sunday"),
+            new DeliveryTime("16:15")
     );
 
     /**
@@ -48,20 +58,22 @@ public class SampleDataUtil {
                 SAMPLE_DELIVERY_ALEX),
             new Person(new Name("Bernice Yu"), new Phone("99272758"), new Email("berniceyu@example.com"),
                 new Address("Blk 30 Lorong 3 Serangoon Gardens, #07-18"),
-                getTagSet("Vegetarian"),
+                getTagSet("NoSeafood", "PeanutAllergy"),
                 SAMPLE_DELIVERY_BERNICE),
             new Person(new Name("Charlotte Oliveiro"), new Phone("93210283"), new Email("charlotte@example.com"),
                 new Address("Blk 11 Ang Mo Kio Street 74, #11-04"),
                 getTagSet()),
             new Person(new Name("David Li"), new Phone("91031282"), new Email("lidavid@example.com"),
                 new Address("Blk 436 Serangoon Gardens Street 26, #16-43"),
-                getTagSet("Vegan")),
+                getTagSet("LactoseIntolerant")),
             new Person(new Name("Irfan Ibrahim"), new Phone("92492021"), new Email("irfan@example.com"),
                 new Address("Blk 47 Tampines Street 20, #17-35"),
-                getTagSet("Halal")),
+                getTagSet("Halal"),
+                SAMPLE_DELIVERY_IRFAN),
             new Person(new Name("Roy Balakrishnan"), new Phone("92624417"), new Email("royb@example.com"),
                 new Address("Blk 45 Aljunied Street 85, #11-31"),
-                getTagSet("Pescatarian"))
+                getTagSet("Vegetarian"),
+                SAMPLE_DELIVERY_ROY)
         };
     }
 
